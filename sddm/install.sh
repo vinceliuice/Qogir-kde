c474b76fa23811ea9644a301be0c5bc5802cb776
Elian Van Cutsem <elian.vancutsem@student.odisee.be>
2020-01-25 18:55:25 +0100

    better if statement

--- sddm/install.sh
+++ sddm/install.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
-if (( $EUID != 0 )); then
+if [[ "$EUID" != 0 ]]; then
   echo "Please run as root"
   exit
 else
-  clear
   echo "Installing theme..."
   cp -r Qogir /usr/share/sddm/themes
-  clear
   echo "Install finished..."
 fi
