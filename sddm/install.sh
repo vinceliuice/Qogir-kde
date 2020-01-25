ac72c1229337e3b9efbfde4b92064073bf52b728
Elian Van Cutsem <elian.vancutsem@student.odisee.be>
2020-01-25 18:49:28 +0100

    better install script

--- sddm/install.sh
+++ sddm/install.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-
-cp -r Qogir /usr/share/sddm/themes
-
-echo "Install finished..."
+if (( $EUID != 0 )); then
+  echo "Please run as root"
+  exit
+else
+  clear
+  echo "Installing theme..."
+  cp -r Qogir /usr/share/sddm/themes
+  clear
+  echo "Install finished..."
+fi
