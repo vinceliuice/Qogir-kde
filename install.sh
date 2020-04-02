c3614335b532f25b431cf0eab2264521dc09bf74
vinceliuice <vinceliuice@hotmail.com>
2020-04-02 12:18:08 +0800

    Add manjaro and ubuntu version and fixed LAF issues

--- install.sh
+++ install.sh
@@ -19,13 +19,6 @@ install() {
   [[ ${color} == '-dark' ]] && local ELSE_DARK=${color} && local c_color=dark
   [[ ${color} == '-light' ]] && local ELSE_LIGHT=${color} && local c_color=light
 
-  local AURORAE_THEME="${AURORAE_DIR}/${name}${color}"
-  local PLASMA_THEME="${PLASMA_DIR}/${name}${color}"
-  local LOOKFEEL_THEME="${LOOKFEEL_DIR}/com.github.vinceliuice.${name}${color}"
-  local SCHEMES_THEME="${SCHEMES_DIR}/${name}${c_color}.colors"
-  local WALLPAPER_THEME="${WALLPAPER_DIR}/${name}/${name}${color}.jpeg"
-  local KVANTUM_THEME="${KVANTUM_DIR}/${name}${color}"
-
   mkdir -p                                                                           ${AURORAE_DIR}
   mkdir -p                                                                           ${SCHEMES_DIR}
   mkdir -p                                                                           ${PLASMA_DIR}
@@ -33,27 +26,20 @@ install() {
   mkdir -p                                                                           ${KVANTUM_DIR}
   mkdir -p                                                                           ${WALLPAPER_DIR}/${name}
 
-  [[ -d ${AURORAE_THEME} ]] && rm -rf ${AURORAE_THEME}
-  [[ -d ${AURORAE_THEME}-circle ]] && rm -rf ${AURORAE_THEME}-circle
-  [[ -d ${PLASMA_THEME} ]] && rm -rf ${PLASMA_THEME}
-  [[ -d ${LOOKFEEL_THEME} ]] && rm -rf ${LOOKFEEL_THEME}
-  [[ -d ${SCHEMES_THEME} ]] && rm -rf ${SCHEMES_THEME}
-  [[ -d ${WALLPAPER_THEME} ]] && rm -rf ${WALLPAPER_THEME}
-  [[ -d ${KVANTUM_THEME} ]] && rm -rf ${KVANTUM_THEME}
-
-  cp -ur ${SRC_DIR}/aurorae/themes/*                                                 ${AURORAE_DIR}
-  cp -ur ${SRC_DIR}/color-schemes/*.colors                                           ${SCHEMES_DIR}
-  cp -ur ${SRC_DIR}/wallpaper/*.jpeg                                                 ${WALLPAPER_DIR}/${name}
-  cp -ur ${SRC_DIR}/Kvantum/*                                                        ${KVANTUM_DIR}
-  cp -ur ${SRC_DIR}/plasma/desktoptheme/${name}${ELSE_DARK}                          ${PLASMA_DIR}
+  cp -r ${SRC_DIR}/aurorae/themes/*                                                  ${AURORAE_DIR}
+  cp -r ${SRC_DIR}/color-schemes/*.colors                                            ${SCHEMES_DIR}
+  cp -r ${SRC_DIR}/wallpaper/*.jpeg                                                  ${WALLPAPER_DIR}/${name}
+  cp -r ${SRC_DIR}/Kvantum/*                                                         ${KVANTUM_DIR}
+  cp -r ${SRC_DIR}/plasma/desktoptheme/${name}${ELSE_DARK}                           ${PLASMA_DIR}
+  cp -r ${SRC_DIR}/plasma/desktoptheme/icons${ELSE_DARK}                             ${PLASMA_DIR}/${name}${ELSE_DARK}/icons
 
   if [[ ${color} == '-dark' ]]; then
-    cp -ur ${SRC_DIR}/color-schemes/${name}dark.colors                               ${PLASMA_DIR}/${name}-dark/colors
+    cp -r ${SRC_DIR}/color-schemes/${name}dark.colors                                ${PLASMA_DIR}/${name}-dark/colors
   else
-    cp -ur ${SRC_DIR}/color-schemes/${name}light.colors                              ${PLASMA_DIR}/${name}/colors
+    cp -r ${SRC_DIR}/color-schemes/${name}light.colors                               ${PLASMA_DIR}/${name}/colors
   fi
 
-  cp -ur ${SRC_DIR}/plasma/look-and-feel/com.github.vinceliuice.${name}${color}      ${LOOKFEEL_DIR}
+  cp -r ${SRC_DIR}/plasma/look-and-feel/com.github.vinceliuice.${name}${color}      ${LOOKFEEL_DIR}
 }
 
 echo "Installing 'Qogir kde themes'..."
